24ec985cd1234357fce55fab3a3deed7b97d65b3
jsoros <jsoros@gmail.com>
2025-08-31 22:04:53 -0400

    main set base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   // @ts-expect-error - Vite config types don't include test property
   test: {
     globals: true,
